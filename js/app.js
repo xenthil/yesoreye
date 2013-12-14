5e4aa5479ee180254d06014c366cae312128dd93
yesoreyeram <yesoreyeram@gmail.com>
2013-12-14 23:17:47 +0530

    Login Screen Added

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ app.config(function($routeProvider)
 	$routeProvider.when("/departments", {templateUrl:"partials/departments.html"});
 	$routeProvider.when("/analytics", {templateUrl:"partials/analytics.html"});
     $routeProvider.when("/complaints", {templateUrl: "partials/complaints.html"});
+    $routeProvider.when("/logout", {templateUrl: "partials/login.html"});
     $routeProvider.otherwise({redirectTo: '/home'});
 });
 
