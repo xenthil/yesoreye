c666d8b95e7430249e962559d43684e05097ea0b
yesoreyeram <yesoreyeram@gmail.com>
2013-12-15 08:33:40 +0530

    baby pages

--- js/app.js
+++ js/app.js
@@ -9,7 +9,10 @@ app.config(function($routeProvider)
 	$routeProvider.when("/departments", {templateUrl:"partials/departments.html"});
 	$routeProvider.when("/analytics", {templateUrl:"partials/analytics.html"});
     $routeProvider.when("/complaints", {templateUrl: "partials/complaints.html"});
-    $routeProvider.when("/logout", {templateUrl: "partials/login.html"});
+    $routeProvider.when("/logout", {templateUrl: "partials/login.html"});    
+    $routeProvider.when("/app/newbaby", {templateUrl: "partials/app/newbaby.html"});
+    $routeProvider.when("/app/searchbaby", {templateUrl: "partials/app/searchbaby.html"});    
+    $routeProvider.when("/app/newmother", {templateUrl: "partials/app/newmother.html"});
     $routeProvider.otherwise({redirectTo: '/home'});
 });
 
@@ -21,7 +24,10 @@ app.controller('OneCMACtrl',function($scope)
 
 app.controller('AppsListController',function($scope)
 {
-	$scope.appsList = [{ name : 'Apply Birth Certificate' , department: 'Sanitation'},
+	$scope.appsList = [{ name : 'Apply Birth Certificate'},
+		{	name : 'Enter New Baby Details' , appid: 'newbaby'},		
+		{	name : 'Search Baby Details' , appid: 'searchbaby'},		
+		{	name : 'Enter New Conceived Mother Details' , appid: 'newmother'},
 		{	name : 'Apply Death Certificate' },
 		{	name : 'Book Marriage Hall' },
 		{	name : 'Book Mortuary' },
@@ -30,6 +36,9 @@ app.controller('AppsListController',function($scope)
 		{name:'New schemes'},
 		{name:'Lodge a Complaint'},
 		{name: 'City Details'},
+		{name:'Ward Details'},
+		{name:'Burial Ground Booking'},
+		{name:'Freezer Box booking'},
 		{name:'Ward Details'},
 		{name:'Events & Programms by CMA'}
 
